aee88c94ac058aa9d9c015ce50d35ddfc78f33df
Brianna Haberman <happyhaberman@yahoo.com>
2025-07-14 23:39:59 +0000

    finished part 2

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         // Part 1: Operator Practice
-        
+
         int a = 10, b = 5, c = 15;
         Console.WriteLine("Integers: a = 10, b = 5, and c = 15.");
         Console.WriteLine("a > b: " + (a > b));
@@ -13,6 +13,26 @@ class Program
         Console.WriteLine("a > b && a > c: " + (a > b && a > c));
         Console.WriteLine("a = b || b < c: " + (a == b || b < c));
         Console.WriteLine("a >= b && c > b: " + (a >= b && c > b));
+
+        // Part 2: Boolean Logic
+
+        bool isRaining = true;
+        bool haveUmbrella = false;
+
+        if (isRaining && !haveUmbrella)
+        {
+            Console.WriteLine("Take an umberella!");
+        }
+        else
+        {
+            Console.WriteLine("You're good to go!");
+        }
+
+        // Part 3: Conditional Logic
+        
+        
+
+        // Part 4: Using a Switch Statement
 
     }
 }
--- Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbc49bc0e7e6418cd923b1c293bd33a80fe9de54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5cba79c104823fff58eccf95aeebeb4326f2714")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
