032cfb0f0589c1f4e5f5192b1db4a7852aa1d91e
Brianna Haberman <happyhaberman@yahoo.com>
2025-07-15 00:07:28 +0000

    Finished Lab

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -29,10 +29,58 @@ class Program
         }
 
         // Part 3: Conditional Logic
-        
-        
+
+        Console.WriteLine("Please enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        if (age < 5)
+        {
+            Console.WriteLine("Ticket is free!");
+        }
+        else if (age >= 5 && age <= 12)
+        {
+            Console.WriteLine("Child Ticket: $5.00");
+        }
+        else if (age >= 13 && age <= 64)
+        {
+            Console.WriteLine("Standard Ticket: $10.00");
+        }
+        else if (age >= 65)
+        {
+            Console.WriteLine("Senior Ticket: $6.00");
+        }
 
         // Part 4: Using a Switch Statement
 
+        Console.WriteLine("Please enter a day of the week using numbers 1-7: ");
+        int dayofweek = Convert.ToInt32(Console.ReadLine());
+
+        switch (dayofweek)
+        {
+            case 1:
+                Console.WriteLine("Monday");
+                break;
+            case 2:
+                Console.WriteLine("Tuesday");
+                break;
+            case 3:
+                Console.WriteLine("Wednesday");
+                break;
+            case 4:
+                Console.WriteLine("Thursday");
+                break;
+            case 5:
+                Console.WriteLine("Friday");
+                break;
+            case 6:
+                Console.WriteLine("Saturday");
+                break;
+            case 7:
+                Console.WriteLine("Sunday");
+                break;
+            default:
+                Console.WriteLine("Invalid day!");
+                break;
+        }
     }
 }
